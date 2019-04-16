9cc5a3d83dd4a030948737b4ef79f528daf0d999
Witt Allen <qwert59@gmail.com>
2019-04-16 18:01:58 -0500

    Update vc.ps1

--- vc.ps1
+++ vc.ps1
@@ -8,8 +8,25 @@
 # How to deal with negative/fraction exponents: https://www.sparknotes.com/math/algebra2/exponentialfunctions/section1/
 # Just make function for notes instead of hardcoding
 
+$C4 = 261.63
+function Get-Distance { }
+function Get-NoteFrequency {
+    Param ([string]$letter, [int]$number)
+    $letter = $letter -replace '#', 'sharp'
+    # replace 'b' with 'flat' if it's not the only letter in the string
+    $letter = $letter.ToUpper()
+    $letter = $letter -replace '\s', ''
+
+    "C","CSHARP", "DFLAT", "D", "DSHARP",
+    "EFLAT", "E", "F", "FSHARP", "GFLAT",
+    "G", "GSHARP", "AFLAT", "A", "ASHARP",
+    "BFLAT", "B"
+
+    return $frequency
+
+}
 # ----- x4
-$C4=261.6
+$C4=261.63
 $Cs4=277.2
 $Db4=$Cs4
 $D4=293
@@ -27,26 +44,9 @@ $As4=466.1
 $Bb4=$As4
 $B4=493.9
 
-# ----- x5
-$C5=523.25
-$Cs5=554.37
-$Db5=$Cs5
-$D5=587.33
-$Ds5=622.25 
-$Eb5=$Ds5
-$E5=659.26
-$F5=659.26
-$Fs5=739.99
-$Gb5=$Fs5
-$G5=783.99
-$Gs5=830.61
-$Ab5=$Gs5
-$A5=880.00
-$As5=932.33
-$Bb5=$As5
-$B5=987.77
 
 $duration=1000 #1 sec
+
 
 [console]::beep($C*2,$duration)
 [console]::beep($D*2,$duration)
